f18334fd4afb3b4e9776444624241eb0d51ac2b3
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-20 21:44:07 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-
+import CostItem from './components/CostItem';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <CostItem />
     </div>
   );
 }
---
+++ CostItem.js
@@ -0,0 +1,19 @@
+import './CostItem.css'
+
+function CostItem(){
+
+    return(
+        <div className='wrapper'>
+            <div>
+                Date
+            </div>
+            <div>
+                Title
+            </div>
+            <div>
+                Sum
+            </div>
+        </div>
+    )
+}
+export default CostItem;
