d00f1fa43cf818eef252b2e8a00c1f9ad057914b
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-23 22:04:49 +0300

    Add files via upload

--- CostItem.js
+++ CostItem.js
@@ -2,10 +2,25 @@ import './CostItem.css'
 
 function CostItem(){
 
+    const date = new Date();
+    // const year = date.getFullYear();
+    // const month = date.getMonth();
+    // const day = date.getDay();
+
+    const options = {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        weekday: 'short',
+        timezone: 'UTC',
+        hour: 'numeric',
+        minute: 'numeric',
+      };
+
     return(
         <div className='cost-item'>
             <div>
-                Date
+                {date.toLocaleString("ru", options)}
             </div>
             <div className='cost-item__description'>
                 <h2>Title</h2>
