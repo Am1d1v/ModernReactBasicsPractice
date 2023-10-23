9773252489547524d8652477b9cc2b953b64abf3
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-23 22:52:38 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -2,9 +2,32 @@ import CostItem from './components/CostItem';
 import './App.css';
 
 function App() {
+
+ const costs = [
+    {
+      date: new Date(),
+      costDescription: 'Order List Item 1',
+      costAmount: 1000
+    },
+
+    {
+      date: new Date(),
+      costDescription: 'Order List Item 2',
+      costAmount: 1250
+    },
+
+    {
+      date: new Date(),
+      costDescription: 'Order List Item 3',
+      costAmount: 1500
+    },
+  ]
+
   return (
     <div className="App">
-      <CostItem />
+      <CostItem date={costs[0].date} description={costs[0].costDescription} amount={costs[0].costAmount} />
+      <CostItem date={costs[1].date} description={costs[1].costDescription} amount={costs[1].costAmount} />
+      <CostItem date={costs[2].date} description={costs[2].costDescription} amount={costs[2].costAmount} />
     </div>
   );
 }
--- CostItem.js
+++ CostItem.js
@@ -1,11 +1,13 @@
 import './CostItem.css'
 
-function CostItem(){
+function CostItem(props){
 
-    const date = new Date();
+    //const date = new Date();
     // const year = date.getFullYear();
     // const month = date.getMonth();
     // const day = date.getDay();
+
+    const {date, description, amount} = props;
 
     const options = {
         year: 'numeric',
@@ -17,18 +19,18 @@ function CostItem(){
         minute: 'numeric',
       };
 
-    const costDescription = 'Order List Item';
-    const costAmount = 1000;
+    // const costDescription = 'Order List Item';
+    // const costAmount = 1000;
 
     return(
         <div className='cost-item'>
             <div>
-                {date.toLocaleString("ru", options)}
+                {date.toLocaleString("eng", options)}
             </div>
             <div className='cost-item__description'>
-                <h2>{costDescription}</h2>
+                <h2>{description}</h2>
                 <div className='cost-item__price'> 
-                {`${costAmount}$`}
+                {`${amount}$`}
             </div>
             </div>
         </div>
