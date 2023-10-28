36cfa590346a48f051e4a9879f295f6959e2cfb3
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-28 20:24:46 +0300

    Add files via upload

--- DiagramBar.js
+++ DiagramBar.js
@@ -1,8 +1,29 @@
+import './DiagramBar.css'
 
 
+function DiagramBar(props){
 
-function DiagramBar(){
+    const {value, maxValue, label} = props;
 
+    let barFillHeight = '0';
 
+    if(maxValue > 0){
+        barFillHeight = Math.round((value / maxValue) * 100) + '%';
+    }
+
+    return(
+        <div className='diagram-bar'>
+            <div className='diagram-bar__inner'>
+                <div className='diagram-bar__fill' style={{
+                    height: barFillHeight
+                }}>
+                    {}
+                </div>  
+            </div>
+            <div className='diagram-bar__label'>
+                {label}
+            </div>
+        </div>
+    )
 }
 export default DiagramBar;
